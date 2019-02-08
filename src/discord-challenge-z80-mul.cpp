796f37984d264c6ce7d6f7bf43322d1dc768f9fa
Perry Harrington <pedward@apsoft.com>
2019-02-07 22:57:04 -0800

    Completion
    All 3 objectives implemented, x86 emulator working.

--- src/discord-challenge-z80-mul.cpp
+++ src/discord-challenge-z80-mul.cpp
@@ -34,7 +34,7 @@ using namespace std;
  *
  * Output: 6260060
  *
- * Bonus 1: Handle signed numbers, the x86 processor uses two's compliment to
+ * Bonus 1: Handle signed numbers, the cpu processor uses two's compliment to
  * represent signed numbers, ref:
  * https://en.wikipedia.org/wiki/Two%27s_complement
  *
@@ -45,9 +45,9 @@ using namespace std;
  * Bonus 2: Implement an interpreter which runs the following machine code
  * (shown in NASM/Intel syntax) and prints out the result:
  *
- * 31 d2                	xor    dx,dx
- * b8 cc ed             	mov    ax,0xedcc
- * 69 d0 78 56          	imul   dx,ax,0x5678
+ * 31 d2                	xor    dx,dx			0x30, d=0, s=1 ram, dx, dx
+ * b8 cc ed             	mov    ax,0xedcc		0xb8+r ax reg imm16/32 (s=1 implied)
+ * 69 d0 78 56          	imul   dx,ax,0x5678		0x68, d=0, s=1 ram, dx, ax, imm16/32
  *
  * The input for Bonus 2 will be the following HEX string:
  *
@@ -63,6 +63,251 @@ using namespace std;
  * d) neat factor
  *
  */
+
+struct x86 {
+	enum { ax, cx, dx, bx, sp, bp, si, di };
+	enum { al, cl, dl, bl, ah, ch, dh, bh };
+	enum { eax, ecx, edx, ebx, esp, ebp, esi, edi };
+	enum { ri, osb, fsb, ram };
+	enum { b8, b16 };
+
+	struct po {					// primary opcode
+		union {
+			struct {			// for instructions longer than 1 byte
+				uint8_t s:1;	// operand size, 0=8bit reg/mem 1=16/32 operand size prefix required for 16bit in protected mode (0x66)
+				uint8_t d:1;	// direction
+				uint8_t op:6;	// operand
+			};
+			struct {			// for 1 byte instructions
+				uint8_t r:3;	// register
+				uint8_t opr:5;	// operand
+			};
+		};
+	};
+
+	struct modrm {				// describes addressing mode and which registers operand uses for destination and source
+		uint8_t rm:3;			// source or destination reg
+		uint8_t reg:3;			// dir = 0, reg is source, dir = 1, reg is dest
+		uint8_t mod:2;			// address mode
+	};
+
+	struct sib {				// scaled index byte mode
+		uint8_t base:3;
+		uint8_t index:3;
+		uint8_t ss:2;
+	};
+
+	union {
+		po opcode;
+		uint8_t	b1;
+	};
+
+	union {
+		modrm mod;
+		uint8_t b2;
+	};
+
+	union {
+		sib SIB;
+		uint8_t b3;
+	};
+
+	uint8_t dest;
+	uint8_t src;
+
+	uint16_t regs[8];
+	uint16_t shadow;
+
+	uint8_t expect;
+	bool tertiary;
+
+};
+
+string printbin32(uint32_t n)
+{
+	string s;
+	for (int i=0;i<32;i++) {
+		s+=(n&(1<<31) ? "1":"0");
+		if ((i&7) == 7 && i<31) s+="_";
+		n<<=1;
+	}
+	return s;
+}
+
+string printbin(uint16_t n)
+{
+	string s;
+	for (int i=0;i<16;i++) {
+		s+=((n&0x8000) ? "1":"0");
+		if ((i&7) == 7 && i<15) s+="_";
+		n<<=1;
+	}
+	return s;
+}
+
+int main_cpu() {
+	x86 cpu;
+	string prog;
+
+	cin >> prog;
+
+	enum { operand, modrm, imm8 } state;
+	const char *st_string[]={"_operand_","_modrm_","_imm8_"};
+	enum { decode, exec } uop;
+
+	uint8_t byte;
+	string h;
+
+	cpu.expect = 1;
+	cpu.tertiary = false;
+
+	for (auto c : prog) {
+		h+=c;
+
+		if (h.size() == 2) {
+			byte = strtol(h.c_str(),NULL,16);
+			h.clear();
+		} else continue;
+
+		cout << "Decoded "<< std::hex << (int)byte <<" state "<<st_string[state]<<endl;
+
+		switch (state) {
+			case operand:
+				cpu.b1 = byte;
+				cout << "opr "<<std::hex<<(int)cpu.opcode.opr<<" op "<<(int)cpu.opcode.op<<endl;
+				switch(cpu.opcode.opr) {
+					case 0x17:
+						cout << "0x17"<<endl;
+						cpu.dest = cpu.opcode.r;
+						cpu.opcode.s = 1;
+						cpu.expect = 2;
+						state = imm8;
+						break;
+					default:
+						switch(cpu.opcode.op) {
+							case 0xC:
+								cout << "0xC"<<endl;
+								cpu.expect = 1;
+								state = modrm;
+								break;
+							case 0x1A:
+								cout << "0x1A"<<endl;
+								cpu.expect = 3;
+								cpu.tertiary = true;
+								cpu.regs[x86::dx]=0;
+								state = modrm;
+								break;
+						}
+
+				}
+				break;
+
+			case modrm:
+				cpu.b2 = byte;
+
+				cout << "CPU dir "<<(int)cpu.opcode.d<<endl;
+				if (cpu.opcode.d) {
+					cpu.dest = cpu.mod.reg;
+					cpu.src = cpu.mod.rm;
+				} else {
+					cpu.dest = cpu.mod.rm;
+					cpu.src = cpu.mod.reg;
+				}
+
+				cout << "mod "<<(int)cpu.mod.mod<<" reg "<<(int)cpu.mod.reg<<" rm "<<(int)cpu.mod.rm<<endl;
+				cout << "CPU src"<<(int)cpu.src<<" dest "<<(int)cpu.dest<<endl;
+				if (cpu.expect) state = imm8;
+
+				break;
+
+			case imm8:
+				cout << "immediate"<<endl;
+				if (cpu.tertiary) {
+					uint16_t temp = (uint16_t)byte << 8;
+					cpu.shadow >>= 8;
+					cpu.shadow |=temp;
+				} else {
+					if (cpu.opcode.s) {
+						uint16_t temp = (uint16_t)byte << 8;
+						cout <<"temp "<<std::hex<<temp<<endl;
+						cpu.regs[cpu.dest]>>=8;
+						cpu.regs[cpu.dest]|=temp;
+					} else {
+						cout <<"byte operand "<<endl;
+						cpu.regs[cpu.dest]=byte;
+					}
+				}
+				break;
+		}
+
+		if (cpu.expect == 0) {
+			state = operand;
+			uop = exec;
+			cpu.tertiary = false;
+		}
+
+		cpu.expect--;
+
+		if (uop == exec) {
+			cout << "Executing..."<<endl;
+			switch (cpu.opcode.op){
+				case 0xC:
+					cpu.regs[cpu.dest] = cpu.regs[cpu.dest] ^ cpu.regs[cpu.src];
+					break;
+				case 0x1A: {
+					bool sign = (cpu.regs[cpu.dest] & 0x8000) ^ (cpu.regs[cpu.src] & 0x8000);	// test if either input value is negative, both negative, or not negative and set sign of result appropriately
+
+					if (cpu.regs[cpu.dest] & 0x8000) cpu.regs[cpu.dest] = ~cpu.regs[cpu.dest] + 1;					// convert from two's complement to unsigned
+					if (cpu.shadow & 0x8000) cpu.shadow = ~cpu.shadow + 1;
+
+					uint16_t a=cpu.regs[cpu.dest];
+					uint16_t carry=0;
+					cpu.regs[cpu.dest]=0;
+
+					while (a && cpu.shadow) {							// fall through if either input is zero or when we're done
+						if (a&1) {								// shift + add, test bit about to be shifted out
+							cpu.regs[cpu.dest] += cpu.shadow;				// if multiplier is 1, we add shifted multiplicand and carry
+							cpu.regs[cpu.src] += carry + ((cpu.shadow&0x8000)>>15);
+						}
+						carry <<= 1;							// carry the multiplicand bits
+						carry += (cpu.shadow&0x8000)>>15;					// since we are doing 16x32 multiply, the multiplicand can't shift past 16 bits, so we have to maintain a carry
+						cpu.shadow <<= 1;								// shift part of shift + add
+						a >>= 1;								// decimate the multiplier for next round
+					}
+
+					if (sign) {
+						cpu.regs[cpu.dest] = ~cpu.regs[cpu.dest];
+						cpu.regs[cpu.src] = ~cpu.regs[cpu.src];
+
+						if (cpu.regs[cpu.dest] < 0xFFFF ) {
+							cpu.regs[cpu.dest]++;
+						} else {
+							cpu.regs[cpu.src]++;
+							cpu.regs[cpu.dest]=0;
+						}
+					}
+				}
+				break;
+			}
+			uop = decode;
+		}
+
+		cout << "AX: "<<std::hex<<cpu.regs[x86::ax]<<endl;
+		cout << "BX: "<<std::hex<<cpu.regs[x86::bx]<<endl;
+		cout << "CX: "<<std::hex<<cpu.regs[x86::cx]<<endl;
+		cout << "DX: "<<std::hex<<cpu.regs[x86::dx]<<endl;
+		cout << "SP: "<<std::hex<<cpu.regs[x86::sp]<<endl;
+		cout << "BP: "<<std::hex<<cpu.regs[x86::bp]<<endl;
+		cout << "SI: "<<std::hex<<cpu.regs[x86::si]<<endl;
+		cout << "DI: "<<std::hex<<cpu.regs[x86::di]<<endl;
+
+	}
+
+	printf("Output DX:%04X :AX:%04X\n",cpu.regs[x86::dx],cpu.regs[x86::ax]);
+
+	return 0;
+}
+
 int main_signed_stripped() {
 	uint32_t input;
 
@@ -98,17 +343,6 @@ int main_signed_stripped() {
 	cout << "Output: "<<std::hex<<result<<endl;
 
 	return 0;
-}
-
-string printbin(uint32_t n)
-{
-	string s;
-	for (int i=0;i<32;i++) {
-		s+=(n&(1<<31) ? "1":"0");
-		if ((i&7) == 7 && i<31) s+="_";
-		n<<=1;
-	}
-	return s;
 }
 
 int main_signed() {
@@ -192,5 +426,5 @@ int main_unsigned() {
 }
 
 int main() {
-	return main_signed_stripped();
+	return main_cpu();
 }
